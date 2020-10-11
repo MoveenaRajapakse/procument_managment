865f698bf3232be5a811ca9a3e1a2bdc5a32d4e5
MOVEENA RAJAPAKSE <moveenapadmapali@gmail.com>
2020-10-11 17:42:24 +0530

    Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
+import NavBar from  './components/NavBar';
+import Login from './components/Login';
+import Landing from './components/Landing';
+import OrderManagement from './components/OrderManagement';
+import PurchaseRequisitions from './components/PurchaseRequisitions';
+
 function App() {
   return (
-    <Router/>
+    <Router>
+      <div className="App">
+        <NavBar/>
+        {/*<Route exact path ="/" component={Landing}/>*/}
+        {/*<div className="container">*/}
+        {/*  <Route exact path ="/login" component={Login}/>*/}
+        {/*  <Route exact path ="/orders" component={OrderManagement}/>*/}
+        {/*  <Route exact path ="/purchase" component={PurchaseRequisitions}/>*/}
+        {/*</div>*/}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import {Link , withRouter} from 'react-router-dom';
+
+class NavBar extends Component {
+
+    logOut(e){
+        e.preventDefault()
+        localStorage.removeItem('usertoken');
+        this.props.history.push(`/`)
+    }
+
+
+
+    render() {
+
+        const loginRegLink = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/login" className="nav-link">
+                        <h5> Login</h5>
+                    </Link>
+                </li>
+            </ul>
+        )
+
+        const userLink = (
+            <ul className="navbar-nav" >
+                <li className="nav-item">
+                    <Link to="/orders" className="nav-link ">
+                        <h5> Order Management</h5>
+                    </Link>
+                </li>
+                &nbsp; &nbsp; &nbsp; &nbsp;
+                <li className="nav-item">
+                    <Link to="/purchase" className="nav-link">
+                        <h5> Purchase Requisitions</h5>
+                    </Link>
+                </li>
+                &nbsp; &nbsp; &nbsp; &nbsp;
+                <li className="nav-item">
+                    <a href="" onClick={this.logOut.bind(this)} className="nav-link">
+                        <h5> Logout</h5>
+                    </a>
+                </li>
+            </ul>
+        )
+
+        return(
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{height: '55px'}}>
+                <a className="navbar-brand text-bold" href="#">Procument Management</a>
+                &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbar1"
+                    aria-controls="navbar1"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbar1">
+                    <ul className="navbar-nav">
+                        <li className="nav-item nav-link">
+                            <Link to="/" className="nav-link">
+                                <h5> Home</h5>
+                            </Link>
+                        </li>
+                    </ul>
+                    {localStorage.usertoken ? userLink: loginRegLink}
+                </div>
+            </nav>
+        )
+
+    }
+}
+
+export default withRouter (NavBar);
