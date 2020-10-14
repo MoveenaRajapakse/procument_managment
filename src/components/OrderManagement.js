03cccde82d16de4b1072f989c77b6d1379cd14fb
MOVEENA RAJAPAKSE <moveenapadmapali@gmail.com>
2020-10-14 09:38:13 +0530

    Order UI

--- src/components/OrderManagement.js
+++ src/components/OrderManagement.js
@@ -2,11 +2,100 @@ import React, {Component} from 'react';
 import OderDetailsTable from "./OderDetailsTable";
 
 class OrderManagement extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            pid : '',
+            materiel : '',
+            quantity : ''
+        }
+    }
+
+
+
     render() {
         return (
-            <div>
-               <OderDetailsTable/>
-            </div>
+            <React.Fragment>
+                <br/>
+                <div class="container">
+                    <h4>Updating Inventory</h4>
+                    <form>
+                        <div className="row">
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="game_name">Order ID</label>
+                                    <input type="text"
+                                           className="form-control"
+                                           name="order_id"
+                                           id="order_id"
+                                           placeholder="Select a Record"
+                                        /*value={this.state.game_name}
+                                        onChange={this.onChange}*//>
+                                </div>
+                            </div>
+
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="game_name">Supplier</label>
+                                    <input type="text"
+                                           className="form-control"
+                                           name="sup"
+                                           id="sup"
+                                           placeholder="Select a Record"
+                                        /*value={this.state.game_name}
+                                        onChange={this.onChange}*//>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="game_name">Materiel</label>
+                                    <input type="text"
+                                           className="form-control"
+                                           name="materiel"
+                                           id="materiel"
+                                           placeholder="Select a Record"
+                                        /*value={this.state.game_name}
+                                        onChange={this.onChange}*//>
+                                </div>
+                            </div>
+
+                            <div className="col">
+                                <div className="form-group">
+                                    <label htmlFor="game_name">Quantity</label>
+                                    <input type="text"
+                                           className="form-control"
+                                           name="qty"
+                                           id="qty"
+                                           placeholder="Select a Record"
+                                        /*value={this.state.game_name}
+                                        onChange={this.onChange}*//>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row" >
+                            <div className="col-md-3">
+                                <button className="btn btn-dark btn-block">Accept</button>
+                            </div>
+
+                            <div className="col-md-3">
+                                <button className="btn btn-dark btn-block">Split</button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+
+                <br/><br/>
+                <h4>Available Inventory Details</h4>
+                    <OderDetailsTable/>
+
+            </React.Fragment>
+
         );
     }
 }
--- src/components/UserFunctions.js
+++ src/components/UserFunctions.js
@@ -7,8 +7,24 @@ export const purchaseRequisitions = user => {
             status : user.status
         })
         .then(res => {
-            console.log("Accepted");
-            window.alert("Accepted");
+            console.log("Status Updated");
+            window.alert("Status Updated");
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+export const updateInventory = user => {
+    return axios
+        .put('/updateInventory',{
+            pid: user.pid,
+            materiel: user.materiel,
+            quantity: user.quantity
+        })
+        .then(res => {
+            console.log("Updated");
+            window.alert("Updated");
         })
         .catch(err => {
             console.log(err);
